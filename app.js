00b117858320110df0db852ac13c1fb590a89de6
Miguel Curbelo <mcurbeloj@gmail.com>
2022-11-08 17:31:24 +0100

    fixes artist search hbs issue that threw error

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 require('dotenv').config();
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const hbs = require('hbs');
-const SpotifyWebApi = require('spotify-web-api-node');
+const kirbyDance = require ('kirby-dance');
+
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
@@ -16,14 +29,27 @@ const spotifyApi = new SpotifyWebApi({
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
-const app = express();
-
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
-
-// setting the spotify-api goes here:
-
 // Our routes go here:
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+let artistSearched
+let searchResult
+
+app.get('/artist-search', (req, res) => {
+    artistSearched = req.query.artistSearch;
+    console.log('req.query', req.query)
+    spotifyApi
+    .searchArtists(artistSearched)
+    .then(data => {
+        console.log (kirbyDance(5))
+        console.log('The received data from the API: ', data.body)
+        searchResult = data.body.artists.items
+    })
+    .then(() => res.render('artist-search-results', {searchResult}))
+    .catch(err => console.log('An error occurred', err));
+})
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
